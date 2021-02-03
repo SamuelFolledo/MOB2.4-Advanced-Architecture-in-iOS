bdf8b942af376e957c24905a9a5b40de54441708
Samuel Folledo <samuelfolledo@gmail.com>
2021-02-03 15:05:36 -0500

    [add] answers to day 5 activity

--- Day 5 - MVVM Starter/mvvm-starter/Song Detail/View/SongDetailViewController.swift
+++ Day 5 - MVVM Starter/mvvm-starter/Song Detail/View/SongDetailViewController.swift
@@ -19,23 +19,30 @@ class SongDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //TODO 1: Tell the VM that the view has laoded
+        //TODO 1: Tell the VM that the view has loaded
+        viewModel.viewDidLoad()
+        setupUI()
     }
     
     private func setupUI() {
-        
         //TODO 1.1: set the values in the storyboard
-
+        artistNameLabel.text = viewModel.artist
+        songImage.image = UIImage(named: viewModel.songImageName)
+        releaseDateLabel.text = viewModel.releaseDate
+        songNameLabel.text = viewModel.songName
         //TODO 1.2: Set the button's text
+        setButtonText()
     }
     
     private func setButtonText(){
         //TODO 1.3: If the song is a fav the button should say "Remove from favorites", "Add to favorites" if not
-        
+        let buttonTitle = viewModel.isFavorite ? "Remove from favorites" : "Add to favorites"
+        favoriteButton.setTitle(buttonTitle, for: .normal)
     }
     
     @IBAction func favoriteButtonPressed(_ sender: Any) {
         //TODO 4: handle the logic when pressed, inform the VM
+        viewModel.markFavoriteButtonPressed()
+        setButtonText()
     }
-    
 }
