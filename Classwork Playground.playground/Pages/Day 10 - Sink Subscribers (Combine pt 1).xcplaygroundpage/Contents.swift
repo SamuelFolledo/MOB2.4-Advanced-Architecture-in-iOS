293a5113037ecbf353995f175cda7a5c7949a34a
Samuel Folledo <samuelfolledo@gmail.com>
2021-02-24 14:04:27 -0500

    [add] day 10 and 11 solutions for classwork

--- Classwork Playground.playground/Pages/Day 10 - Sink Subscribers (Combine pt 1).xcplaygroundpage/Contents.swift
+++ Classwork Playground.playground/Pages/Day 10 - Sink Subscribers (Combine pt 1).xcplaygroundpage/Contents.swift
@@ -1,7 +1,25 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
+import Combine
 
-var str = "Hello, playground"
+let sampleNotification = Notification.Name("sampleNotification")
+let publisher = NotificationCenter.default
+    .publisher(for: sampleNotification, object: nil)
+
+let center = NotificationCenter.default
+
+// 1 - Creating the subscription using sink
+let subscription = publisher
+    .sink { _ in
+        print("Notification received using a publisher 📣")
+    }
+
+// 2 - Posting notification
+center.post(name: sampleNotification, object: nil)
+
+// 3 - Cancel subscription
+subscription.cancel()
+
 
 //: [Next](@next)
--- Classwork Playground.playground/Pages/Day 11 - CurrentValueSubject - Subjects (Combine pt 2).xcplaygroundpage/Contents.swift
+++ Classwork Playground.playground/Pages/Day 11 - CurrentValueSubject - Subjects (Combine pt 2).xcplaygroundpage/Contents.swift
@@ -1,7 +1,21 @@
 //: [Previous](@previous)
 
 import Foundation
+import Combine
 
-var str = "Hello, playground"
+// 1- Creating a set
+var subscriptions = Set<AnyCancellable>()
+
+// 2 - This will publish integers, never an error, initial value is 0
+let subject = CurrentValueSubject<Int, Never>(0)
+
+// 3 - Creating a subscription to print received values
+subject
+    .print()
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions) // 4 - Store it in the set
+
+subject.send(1)
+subject.send(2)
 
 //: [Next](@next)
